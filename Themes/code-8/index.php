3b70998073f92e9a6186877f13095dc985e2007d
n0x5 <n0x5@users.noreply.github.com>
2022-11-10 15:06:57 +0100

    Update index.php

--- Themes/code-8/index.php
+++ Themes/code-8/index.php
@@ -8,7 +8,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <div class="post">
-<h3 style="display:inline;"><?php the_date(); ?> - </h3><h2 style="display:inline;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><br>
+<h3 style="display:inline;"><?php get_the_date(); ?> - </h3><h2 style="display:inline;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><br>
 <?php wp_link_pages('before=Sections:&next_or_number=number&pagelink=Page %'); ?>
 <?php the_content(); ?>
 </div>
