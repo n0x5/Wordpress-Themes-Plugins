b75c04418a09d7d88cacc71de88edc44d93f2ed2
n0x5 <n0x5@users.noreply.github.com>
2023-08-18 20:36:30 +0200

    updates to code and db

--- Gutenberg-Blocks/n0x-gptdb/index.php
+++ Gutenberg-Blocks/n0x-gptdb/index.php
@@ -8,7 +8,7 @@ function n0x_gpt_register_block( $block_attributes, $content ) {
 
 $dir = 'sqlite:/home/coax/websites/secondsight/wp-content/gpt.db';
 $dbh  = new PDO($dir, null, null, [PDO::SQLITE_ATTR_OPEN_FLAGS => PDO::SQLITE_OPEN_READONLY]) or die("cannot open the database");
-$query = "select distinct(main_question) from prompt_suggestions order by main_question asc";
+$query = "select distinct(lower(main_question)) from prompt_suggestions order by main_question collate NOCASE";
 
 $cart = array();
 foreach ($dbh->query($query) as $row) {
@@ -18,11 +18,11 @@ foreach ($dbh->query($query) as $row) {
 
 
 foreach ($cart as $quest) {
-    $query2 = "select question, answer, followup from prompt_suggestions where main_question like '".$quest."' order by question asc";
+    $query2 = "select question, answer, followup, followupanswer, keywords from prompt_suggestions where main_question like '".$quest."'";
     $stuff = $stuff . '<!-- wp:details {"summary":'.$quest.'"} -->
-        <details class="wp-block-details"><summary>'.$quest.'</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->';
+        <details class="wp-block-details"><summary>'.ucfirst($quest).'</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->';
     foreach ($dbh->query($query2) as $row2) {
-            $stuff = $stuff . '<p class="question2">' . $row2[0]   .'</p>' . '<p class="answer2">' . $row2[1]   .'</p>' . '<p class="followup2">' . $row2[2]   .'</p>';
+            $stuff = $stuff . '<div class="questionmain"><p class="question2">' . $row2[0]   .'</p>' . '<p class="answer2">' . $row2[1]   .'</p>' . '<p class="followup2">' . $row2[2]   .'</p><p class="followup2">' . $row2[3]   .'</p><p class="followup3">' . $row2[4]   .'</p></div>';
         }
         $stuff = $stuff . '<!-- /wp:paragraph --></details><!-- /wp:details -->';
 }
